b36277c26b6e0f02d7d602a9574f039d6fe6a82e
jchan6532 <82909011+jchan6532@users.noreply.github.com>
2024-05-14 13:10:10 -0400

    created db context: app data context

--- backend/Data/AppDataContext.cs
+++ backend/Data/AppDataContext.cs
@@ -1,15 +1,25 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace backend.Data
 {
     public class AppDataContext : DbContext
     {
-        protected override void OnConfiguring() {
-
+        protected readonly IConfiguration Configuration;
+    
+        public AppDataContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
         }
+
+        public class AppDataContext : DbContext
+        {
+            protected override void OnConfiguring(DbContextOptionsBuilder options){
+                options.UseNpgsql(Configuration.GetConnectionString(""));
+            }
+        }
+
+        public DbSet<> Recipes { get; set; }
+        public DbSet<> Users { get; set; }
     }
 }

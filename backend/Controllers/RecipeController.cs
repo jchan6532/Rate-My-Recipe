a43c32a455bfcc4a17f7bef0206f285e9a3b865f
jchan6532 <82909011+jchan6532@users.noreply.github.com>
2024-05-15 03:09:29 -0400

    refactored controller: recipe controller, added dependency injection and implemented create recipe

--- backend/Controllers/RecipeController.cs
+++ backend/Controllers/RecipeController.cs
@@ -1,27 +1,43 @@
 using Microsoft.AspNetCore.Mvc;
+using backend.Data;
+using Microsoft.EntityFrameworkCore;
+using backend.Models;
 
-namespace backend.Controllers;
+namespace backend.Controllers{
 
-[ApiController]
-[Route("[controller]")]
-public class RecipeController : ControllerBase
-{
-    private readonly ILogger<RecipeController> _logger;
-
-    public RecipeController(ILogger<RecipeController> logger)
+    [ApiController]
+    [Route("[controller]")]
+    public class RecipeController : ControllerBase
     {
-        _logger = logger;
-    }
+        private readonly ILogger<RecipeController> _logger;
+        private readonly IDbContext _context;
 
-    [HttpGet(template: "all", Name = "GetAllRecipes")]
-    public IActionResult Get()
-    {
-        return new ObjectResult(new {a = 1});
-    }
+        public RecipeController(ILogger<RecipeController> logger, IDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
 
-    [HttpGet(template: "none", Name = "GetNoneRecipes")]
-    public IActionResult Getnone()
-    {
-        return new ObjectResult(new {a = 0});
+        [HttpGet(template: "all", Name = "GetAllRecipes")]
+        public async Task<IActionResult> GetAllRecipes()
+        {
+            var recipes = _context.Recipes.ToListAsync();
+            return Ok(new {recipes = recipes});
+        }
+
+        [HttpPost(template: "new", Name = "InsertRecipe")]
+        public async Task<IActionResult> InsertRecipe(Recipe recipe)
+        {
+            _context.Recipes.Add(recipe);
+            int result = await _context.SaveChangesAsync();
+            if (result != 1) return BadRequest(result);
+            return NoContent();
+        }
+
+        [HttpGet(template: "none", Name = "GetNoneRecipes")]
+        public IActionResult Getnone()
+        {
+            return new ObjectResult(new {a = 0});
+        }
     }
 }
